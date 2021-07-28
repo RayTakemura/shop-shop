3f840d73c1335ee6e474ceaedfa280d98561da1c
unknown <raytakemura@gmail.com>
2021-07-27 19:25:08 -0700

    cart ui

---
+++ client/build/precache-manifest.16b8c87fdbf0ee664ecc1414bedd7b24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d0409b784270c110dd5c0907a54f62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "703ffcbd9f648abbbacc",
+    "url": "/static/css/main.b39ba752.chunk.css"
+  },
+  {
+    "revision": "307391235c3c16385b23",
+    "url": "/static/js/2.2cad12d8.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.2cad12d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "703ffcbd9f648abbbacc",
+    "url": "/static/js/main.6f94681e.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
